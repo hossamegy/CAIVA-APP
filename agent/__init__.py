d65d20639e7471d1c06d182b95d4825099beef60
Hossam Eldin <hossam.eldinegy@gmail.com>
2024-09-25 23:18:17 +0300

    updata main

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,3 +1,2 @@
 from .vectore_store.create_vector_store import CreateVectorStore
 from .caivaAgent import Caiva
-import tools
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from agent.caivaAgent import Caiva
+from controllers.agent_controller import agent_router
 app = FastAPI()
 
 abot = Caiva()
+app.include_router(agent_router)
 
 @app.get('/')
 def get_welcome():
