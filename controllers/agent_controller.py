82a1dc8fe5e4957bd639e716be00da5365584223
Hossam Eldin <hossam.eldinegy@gmail.com>
2024-09-26 00:24:25 +0300

    updata controllers

---
+++ controllers/agent_controller.py
@@ -0,0 +1,13 @@
+from agent.caivaAgent import Caiva
+from langchain_core.messages import HumanMessage
+
+
+def return_agent_response(input: str, agent: Caiva) -> dict:
+
+    messages = [HumanMessage(content=input)]
+
+    result = agent.agentGraph.invoke({"messages": messages})
+    
+    return {
+        "message": result['messages'][-1].content
+    }
