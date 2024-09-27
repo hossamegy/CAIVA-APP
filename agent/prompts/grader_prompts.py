b56fc935716ffce533da2eef3976039f60120125
Hossam Eldin <hossam.eldinegy@gmail.com>
2024-09-27 07:25:29 +0300

    update prompts

--- agent/prompts/grader_prompts.py
+++ agent/prompts/grader_prompts.py
@@ -7,7 +7,7 @@ def get_grade_prompot():
             You have six tools at your disposal: 
             1. 'agent' — Use this tool when the user's question relates to agent-specific information.
             2. 'user' — Use this tool for questions about user-specific information or queries related to user details.
-            3. 'book' — Use this tool when the question pertains to book-related inquiries.
+            3. 'book' — Use this tool when the question pertains to book-related inquiries or some specific domains.
             4. 'eye' — Use this tool for questions related to vision or eye-related inquiries.
             5. 'emotions' — Use this tool for questions concerning feelings or emotional states.
 
@@ -16,7 +16,7 @@ def get_grade_prompot():
             Your task is to analyze the question and decide which tool is best suited for the user's needs:
             - If the question pertains to agent-related information, return 'agent'.
             - If the question relates to user information, return 'user'.
-            - If the question involves books or literature, return 'book'.
+            - If the question involves books, literature or some domains, return 'book'.
             - If the question involves eye-related inquiries, return 'eye'.
             - If the question involves emotions, return 'emotions'.
             Make sure to provide a clear and accurate decision.
