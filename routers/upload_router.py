e5654feed91d834ee01b06cd5b8c7bf40fa4cc3c
Hossam Eldin <hossam.eldinegy@gmail.com>
2024-09-28 09:32:04 +0300

    update upload router

--- routers/upload_router.py
+++ routers/upload_router.py
@@ -1,16 +1,16 @@
 from fastapi import APIRouter, UploadFile, status, Depends
 from fastapi.responses import JSONResponse
-from controllers import UploadController
+from controllers import UploadController, VectorStoreController
 from models.enum import ResponesUploadSignal
 from settings import get_settings, Settings
 import aiofiles
-
+import os
 
 
 upload_router = APIRouter()
 
-@upload_router.post('/uplaod/{id}')
-async def upload_file(id: str, file: UploadFile, settings: Settings = Depends(get_settings)):
+@upload_router.post('/uplaod/')
+async def upload_file(file: UploadFile, settings: Settings = Depends(get_settings)):
     upload_controller = UploadController()
     is_valid, response_signal = upload_controller.check_validation_upload_file(
         file=file
@@ -20,13 +20,13 @@ async def upload_file(id: str, file: UploadFile, settings: Settings = Depends(ge
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content={
-                'signal': response_signal
+                'signal': response_signal,
             }
         )
 
     file_path, id = upload_controller.generate_unique_file_name(
         orignal_file_name=file.filename,
-        id=id
+    
     )
     try:
         async with aiofiles.open(file_path, 'wb') as f:
@@ -35,13 +35,15 @@ async def upload_file(id: str, file: UploadFile, settings: Settings = Depends(ge
     except Exception as e:
             return   JSONResponse(
                 content={
-                    'signal': ResponesUploadSignal.FILE_UPLOAD_FAILED
+                    'signal': ResponesUploadSignal.FILE_UPLOAD_FAILED.value,
                 }
             )
-    
+ 
+    VectorStoreController().create_vector_store(os.path.abspath(file_path))
+
     return JSONResponse(
          content={
-            'signal': ResponesUploadSignal.FILE_UPLOAD_SUCCESS,
-            'file_id': id
+            'signal': ResponesUploadSignal.FILE_UPLOAD_SUCCESS.value,
+            'file_id': id,
         }
     ) 
